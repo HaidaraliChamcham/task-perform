93afd459f4f2d8f651521fbd66a9c2f1f60e5103
Haidarali <haideralichamcham98.com>
2020-12-25 06:00:52 -0500

    first commit

--- src/screen/demo.js
+++ src/screen/demo.js
@@ -2,38 +2,51 @@ import React, { useState } from 'react'
 
 export default function Demo() {
   const [incrementBy, setIncrementBy] = useState('');
-  const [subjectNumber, setSubjectNumber] = useState(100);
-  
+  const [subjectNumber, setSubjectNumber] = useState('100');
+
 
   var submitHandler = (e) => {
     e.preventDefault();
     setSubjectNumber(Number(subjectNumber) + Number(incrementBy));
   }
   const checker = (e) => {
-    if(!e.target.value) {
-      setSubjectNumber(100);
+    if (!e.target.value) {
+      setSubjectNumber('100');
     }
   }
   return (
     <div className="container">
+      <h1>Subject Number Updation Task</h1>
       <div className="row">
-        <div className="col box-background">
-          <form className="form" onSubmit={submitHandler}>
-            <select onChange={(e) => setIncrementBy(e.target.value)} value={incrementBy}>
-              <option > select</option>
-              <option value="1">+1</option>
-              <option value="2">+2</option>
-              <option value="-1">-1</option>
-              <option value="-2">-2</option>
-            </select><br></br>
-            <input type="number" id="no" value={subjectNumber}  onBlur={checker} onChange={(e) => setSubjectNumber(e.target.value)}></input><br />
-
-            <button onClick={submitHandler}>Execute</button>
-          </form>
+        <div className="col-sm-6">
+          <div className="card">
+            <div className="card-body">
+              <form className="form-group" onSubmit={submitHandler}>
+                <select  className="form-control" onChange={(e) => setIncrementBy(e.target.value)} value={incrementBy}>
+                  <option value=""> Please Select</option>
+                  <option value="1">+1</option>
+                  <option value="2">+2</option>
+                  <option value="-1">-1</option>
+                  <option value="-2">-2</option>
+                </select><br></br>
+                <input type="number" className="form-control" id="no" value={subjectNumber} onBlur={checker} onChange={(e) => setSubjectNumber(e.target.value)}></input>
+                <br />
+               <button className="btn btn-primary" onClick={submitHandler}>Execute</button>
+              </form>
+            </div>
+          </div>
         </div>
-        <div className="col box-background">
-          Subject Number =  {subjectNumber};
-    </div>
+
+
+        <div className="col-sm-6"> 
+          <div className="card">
+            <div className="card-body">
+              <div className="card-title center align-middle">
+                Subject Number =  {subjectNumber};
+         </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
